933d35887c6f989be83770c90d23d4f31835b3bf
GreilynES <greilynesquivel@gmail.com>
2026-03-14 13:32:45 -0600

    boton

--- app/page.tsx
+++ app/page.tsx
@@ -803,13 +803,6 @@ export default function MizuPage() {
           </div>
         </div>
       </footer>
-
-      {/* Back to Top */}
-      <button className="back-to-top" aria-label="Back to top">
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="m18 15-6-6-6 6" />
-        </svg>
-      </button>
     </>
   )
 }
